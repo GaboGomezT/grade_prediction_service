a2df62732d1f19b13745d34d17dd833c4467bdea
GaboGomez09 <gomezgabriel09@gmail.com>
2020-11-15 21:15:18 -0600

    Feature: loads model return grade prediction

--- ml_api/main.py
+++ ml_api/main.py
@@ -1,9 +1,15 @@
+import pickle
+
+import pandas as pd
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 app = FastAPI()
+model = pickle.load(open("model.p", "rb"))
+
 
 class Answer(BaseModel):
     age: int
@@ -17,10 +23,18 @@ class Answer(BaseModel):
     health: int
     absences: int
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.post("/grade_predict")
 async def create_item(answer: Answer):
-    return answer
+    answer_dict = jsonable_encoder(answer)
+    for key, value in answer_dict.items():
+        answer_dict[key] = [value]
+    # answer_dict = {k:[v] for (k,v) in jsonable_encoder(answer).items()} # Pythonic One liner version (using dict comprehensions)
+    single_instance = pd.DataFrame.from_dict(answer_dict)
+    prediction = model.predict(single_instance)
+    return prediction[0]
